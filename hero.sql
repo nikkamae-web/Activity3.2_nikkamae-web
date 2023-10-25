5e1e94b9895d2e0faee30e7723b700bdea490a95
Nikka Caldito <nikkacaldito6@gmail.com>
2023-10-25 13:47:41 +0800

    Update hero state to inactive and delete associated item

--- hero.sql
+++ hero.sql
@@ -157,3 +157,11 @@ VALUES
 -- Add the 'item_price' column to the 'item' table
 ALTER TABLE item
 ADD item_price DECIMAL(10, 2);
+-- Update the state of hero 1 to inactive
+UPDATE hero
+SET is_active = false
+WHERE hero_id = 1;
+
+-- Delete the item associated with hero 1
+DELETE FROM heroitem
+WHERE hero_id = 1;
